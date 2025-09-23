54b0b2f47dded010b9fabb5ff31f32431ac32301
Tushar Verma <tusharmyself06@gmail.com>
2025-09-23 23:44:07 +0530

    updated metro config for web

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,11 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
+// Ensure the config works for web
+config.resolver.platforms = ["native", "web", "ios", "android"];
+
+config.resolver.alias = {
+  "react-native$": "react-native-web",
+};
+
 module.exports = config;
